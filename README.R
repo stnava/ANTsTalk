66f81ade390b1850273a882802cc6c629932d0dd
BBAvants <stnava@gmail.com>
2014-09-02 11:44:20 -0400

    ENH: Major reorganization of Rmd

--- README.R
+++ README.R
@@ -7,16 +7,20 @@ library(shiny)
 library(ggvis)
 library(rmarkdown)
 srcdir<-"./"
-pre='antsHistory'
-render( paste(pre,".Rmd",sep=''), clean=TRUE, "revealjs_presentation", output_file='index.html' ) 
+buildrmd='antstalkbuild.Rmd'
+allfns<-Sys.glob("*/*Rmd")
+# print(allfns) # look these over to see the organization you want
+rawrmds<-c("firstslide.Rmd","background/background1.Rmd","registration/registration1.Rmd","templates/templates1.Rmd",
+           "segmentation/segmentation1.Rmd","inhomogeneitycorrection/inhomogeneitycorrection1.Rmd","jlf/jlf1.Rmd",
+           "evaluation/sata.Rmd","evaluation/antsvfreesurfer.Rmd","antsr/antsrdemo.Rmd",
+           "discussion/discussion1.Rmd","lastslide.Rmd")
+for ( x in 1:length(rawrmds) ) {
+  if ( x == 1 )  cmd<-paste( "cat ",rawrmds[x]," > ",buildrmd ) else cmd<-paste( "cat ",rawrmds[x]," >> ",buildrmd )
+  system(cmd)
+}
+render(buildrmd, clean=TRUE, "revealjs_presentation", output_file='index.html' ) 
+#
 # render( paste(pre,".Rmd",sep=''), clean=TRUE, "pdf_document" )
 # render( paste(pre,".Rmd",sep=''), clean=TRUE, "ioslides_presentation" ) 
 # render( paste(pre,".Rmd",sep=''), clean=TRUE, "beamer_presentation" ) 
-
-ss<-"Im interested in your brain alignment algorithms, and why you think
-they are the best.  I'm also interested in whether your software
-implementations will scale up to whole brain microscopy datasets, which
-will be much larger than MRI datasets.
-
- highlighting and explaining the fundamental
-computational problems will be important."
+#
